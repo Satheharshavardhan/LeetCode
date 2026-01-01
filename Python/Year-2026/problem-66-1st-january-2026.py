dba65d723c04075009a45c34ec69ddfcd76ea1f9
SatheHarshavardhan <honeyharshu0125@gmail.com>
2026-01-01 12:19:32 +0530

    Added Problem 66 (1st Jan, 2026)

---
+++ Python/Year-2026/problem-66-1st-january-2026.py
@@ -0,0 +1,13 @@
+from typing import List
+
+
+def solution66(digits: List[int]) -> List[int]:
+    num = "".join(str(i) for i in digits)
+    num_int = int(num) + 1
+    num = str(num_int)
+    result = [int(i) for i in num]
+    return result
+
+
+if __name__ == "__main__":
+    solution66([1, 2, 3])
